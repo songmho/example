13940f859cefebd8cee81c732eb1e61af082b070
songmho <songmho@gmail.com>
2015-05-11 19:06:39 +0900

    first

---
+++ MFCApplication3/MFCApplication3View.cpp
@@ -0,0 +1,96 @@
+
+// MFCApplication3View.cpp : CMFCApplication3View 클래스의 구현
+//
+
+#include "stdafx.h"
+// SHARED_HANDLERS는 미리 보기, 축소판 그림 및 검색 필터 처리기를 구현하는 ATL 프로젝트에서 정의할 수 있으며
+// 해당 프로젝트와 문서 코드를 공유하도록 해 줍니다.
+#ifndef SHARED_HANDLERS
+#include "MFCApplication3.h"
+#endif
+
+#include "MFCApplication3Doc.h"
+#include "MFCApplication3View.h"
+
+#ifdef _DEBUG
+#define new DEBUG_NEW
+#endif
+
+
+// CMFCApplication3View
+
+IMPLEMENT_DYNCREATE(CMFCApplication3View, CView)
+
+BEGIN_MESSAGE_MAP(CMFCApplication3View, CView)
+	ON_WM_CONTEXTMENU()
+	ON_WM_RBUTTONUP()
+END_MESSAGE_MAP()
+
+// CMFCApplication3View 생성/소멸
+
+CMFCApplication3View::CMFCApplication3View()
+{
+	// TODO: 여기에 생성 코드를 추가합니다.
+
+}
+
+CMFCApplication3View::~CMFCApplication3View()
+{
+}
+
+BOOL CMFCApplication3View::PreCreateWindow(CREATESTRUCT& cs)
+{
+	// TODO: CREATESTRUCT cs를 수정하여 여기에서
+	//  Window 클래스 또는 스타일을 수정합니다.
+
+	return CView::PreCreateWindow(cs);
+}
+
+// CMFCApplication3View 그리기
+
+void CMFCApplication3View::OnDraw(CDC* /*pDC*/)
+{
+	CMFCApplication3Doc* pDoc = GetDocument();
+	ASSERT_VALID(pDoc);
+	if (!pDoc)
+		return;
+
+	// TODO: 여기에 원시 데이터에 대한 그리기 코드를 추가합니다.
+}
+
+void CMFCApplication3View::OnRButtonUp(UINT /* nFlags */, CPoint point)
+{
+	ClientToScreen(&point);
+	OnContextMenu(this, point);
+}
+
+void CMFCApplication3View::OnContextMenu(CWnd* /* pWnd */, CPoint point)
+{
+#ifndef SHARED_HANDLERS
+	theApp.GetContextMenuManager()->ShowPopupMenu(IDR_POPUP_EDIT, point.x, point.y, this, TRUE);
+#endif
+}
+
+
+// CMFCApplication3View 진단
+
+#ifdef _DEBUG
+void CMFCApplication3View::AssertValid() const
+{
+	CView::AssertValid();
+}
+
+void CMFCApplication3View::Dump(CDumpContext& dc) const
+{
+	CView::Dump(dc);
+}
+
+CMFCApplication3Doc* CMFCApplication3View::GetDocument() const // 디버그되지 않은 버전은 인라인으로 지정됩니다.
+{
+	ASSERT(m_pDocument->IsKindOf(RUNTIME_CLASS(CMFCApplication3Doc)));
+	return (CMFCApplication3Doc*)m_pDocument;
+}
+#endif //_DEBUG
+
+
+// CMFCApplication3View 메시지 처리기
